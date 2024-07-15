5eb0a1c976f827a801ab8780ef6b759045340d1a
91leejs <lgchoco@naver.com>
2024-07-15 17:48:07 +0900

    hollys1

---
+++ js/hollys.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".main > li, .sub_bg").hover(function(){
+        $(".sub, .sub_bg").stop().slideDown(300);
+    },
+    function(){
+        $(".sub, .sub_bg").stop().slideUp(300);
+
+    })//hover
+
+    $(".move").slick({
+        autoplay:true,
+        autoplayTime:1000,
+        dots:true,
+        arrows:false,
+        adaptiveHeight:true,
+    })//move slick
+})//jQuery
